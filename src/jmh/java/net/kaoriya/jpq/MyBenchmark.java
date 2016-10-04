7617e808f7f6190494d0291e03e694104fbef58f
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-10-04 12:59:11 +0900

    benchmark skeleton

---
+++ src/jmh/java/net/kaoriya/jpq/MyBenchmark.java
@@ -0,0 +1,19 @@
+package net.kaoriya.jpq;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+
+public class MyBenchmark {
+
+    @Benchmark @BenchmarkMode(Mode.Throughput)
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+    }
+
+}
